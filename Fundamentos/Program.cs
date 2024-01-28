818334f5417027b48b16317d6fd3ebe539e12ac3
Brasilino de Souza Neto <neto.bq@gmail.com>
2024-01-28 18:33:52 -0300

    study
    study

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,73 +1,102 @@
-﻿// // --------------------  Array e lista -----------------------
-
-
-// List<string> listaString = new List<string>();
-
-// listaString.Add("SC");
-// listaString.Add("RS");
-// listaString.Add("PR");
-
-// System.Console.WriteLine("UTILIZANDO FOREACH");
-// int contador1 = 0;
-// foreach (var Lista in listaString)
-// {
-//     System.Console.WriteLine($"Posição {contador1} - {listaString[contador1]} "); 
-//     contador1++;
-// }
-
-// System.Console.WriteLine("UTILIZANDO FOR");
-// for (int contador = 0; contador < listaString.Count; contador++)
-// {
-//     System.Console.WriteLine($"Posição {contador} - {listaString[contador]} ");    
-// }
-
-
-// int[] arrayInteiros = new int [3];
-
-// arrayInteiros[0] = 72;
-// arrayInteiros[1] = 64;
-// arrayInteiros[2] = 50;
-
-
-// int [] arrayInteirosDobrados = new int [arrayInteiros.Length * 2];
-// Array.Copy (arrayInteiros, arrayInteirosDobrados, arrayInteiros.Length );
-
-
-// // Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
-
-// for (int contador = 0; contador < arrayInteiros.Length; contador++)
-// {
-//     System.Console.WriteLine($"Posição numero {contador} - {arrayInteiros[contador]}");   
-// }
-
-// foreach (int valor in arrayInteiros)
-// {
-//     System.Console.WriteLine(valor);
-// }
+﻿
 
 
 
 
-// // ----------- WHILE --------------------
-
-
-// int numero = 10;
-// int contador = 0;
-
-// while (numero >= contador )
-// {
-//     Console.WriteLine($"Contador está no numero {contador}");
-//     contador++;
-// }
 
 
 
+// double pi, raio, volume;
+//     pi = 3.14159;
+//  //TODO: Complete os espaços em branco com uma possível solução para o desafio     
+//     raio = Convert.ToDouble(Console.ReadLine());
+//     volume = pi * (  4.0/3    ) * Math.Pow( raio , 3 );
+    
+//     Console.WriteLine($"VOLUME = {volume.ToString("F3")}       ");
+  
 
-// ------------- FOR ---------------------
 
-// int numero = 10;
 
-// for (int contador = 0; contador <= numero; contador++)
-// {
-//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
-// }
+
+
+
+
+
+
+
+
+
+
+// // // --------------------  Array e lista -----------------------
+
+
+// // List<string> listaString = new List<string>();
+
+// // listaString.Add("SC");
+// // listaString.Add("RS");
+// // listaString.Add("PR");
+
+// // System.Console.WriteLine("UTILIZANDO FOREACH");
+// // int contador1 = 0;
+// // foreach (var Lista in listaString)
+// // {
+// //     System.Console.WriteLine($"Posição {contador1} - {listaString[contador1]} "); 
+// //     contador1++;
+// // }
+
+// // System.Console.WriteLine("UTILIZANDO FOR");
+// // for (int contador = 0; contador < listaString.Count; contador++)
+// // {
+// //     System.Console.WriteLine($"Posição {contador} - {listaString[contador]} ");    
+// // }
+
+
+// // int[] arrayInteiros = new int [3];
+
+// // arrayInteiros[0] = 72;
+// // arrayInteiros[1] = 64;
+// // arrayInteiros[2] = 50;
+
+
+// // int [] arrayInteirosDobrados = new int [arrayInteiros.Length * 2];
+// // Array.Copy (arrayInteiros, arrayInteirosDobrados, arrayInteiros.Length );
+
+
+// // // Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+// // for (int contador = 0; contador < arrayInteiros.Length; contador++)
+// // {
+// //     System.Console.WriteLine($"Posição numero {contador} - {arrayInteiros[contador]}");   
+// // }
+
+// // foreach (int valor in arrayInteiros)
+// // {
+// //     System.Console.WriteLine(valor);
+// // }
+
+
+
+
+// // // ----------- WHILE --------------------
+
+
+// // int numero = 10;
+// // int contador = 0;
+
+// // while (numero >= contador )
+// // {
+// //     Console.WriteLine($"Contador está no numero {contador}");
+// //     contador++;
+// // }
+
+
+
+
+// // ------------- FOR ---------------------
+
+// // int numero = 10;
+
+// // for (int contador = 0; contador <= numero; contador++)
+// // {
+// //     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// // }
--- Fundamentos/obj/Debug/net8.0/Fundamentos.AssemblyInfo.cs
+++ Fundamentos/obj/Debug/net8.0/Fundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4561447e1dcbcc8e88f04ca61335ae2dadee3b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee6028b13c4aaa0e918e0711ea918e333fef24aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
