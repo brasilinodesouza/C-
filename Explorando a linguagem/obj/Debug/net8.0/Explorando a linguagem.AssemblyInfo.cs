dd3ce5324deb2a90f3f662781f672dd0fb144f2a
Brasilino de Souza Neto <neto.bq@gmail.com>
2024-02-05 20:33:52 -0300

    Study
    Study

--- Explorando a linguagem/obj/Debug/net8.0/Explorando a linguagem.AssemblyInfo.cs
+++ Explorando a linguagem/obj/Debug/net8.0/Explorando a linguagem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Explorando a linguagem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec3c9104ceb271685e297e198532a0ba95b541d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c83b163ba53415858f3e75167cfc2de605ad7bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Explorando a linguagem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Explorando a linguagem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
